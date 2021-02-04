547b71272dfcb313ce1aa02312e709d960c412cf
PARADOX-RG <vkurtik12@gmail.com>
2021-02-04 18:24:26 +0200

    added product list 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,29 @@
 $(function(){
 
+    $('.star').rateYo({
+        starWidth: "11px",
+        normalFill: "#d6d6d6",
+        ratedFill: "#ffcc00",
+        readOnly: true
+    });
+
+    $('.filter__price-input').ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 100.00,
+        to: 500.00,
+        prefix: "$",
+        onStart: function (data){
+            $('.filter__price-from').text(data.from);
+            $('.filter__price-to').text(data.to);
+        },
+        onChange: function (data) {
+            $('.filter__price-from').text(data.from);
+            $('.filter__price-to').text(data.to);
+        },
+    });
+
     $('.top-slider__inner').slick({
         dots: true,
         arrows: false,
