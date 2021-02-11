e6cf20bb2b928ddfeb4ad8d37c63dd4ccdb5e829
PARADOX-RG <vkurtik12@gmail.com>
2021-02-11 14:37:17 +0200

    added blog

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,41 @@
 $(function(){
 
+    $('.related-content').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed: 300,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/angel-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/icons/angel-next.svg" alt=""></button>'
+        
+    });
+
+    $('.details-tabs__top-item').on('click', function(e){
+        e.preventDefault();
+        $('.details-tabs__top-item').removeClass('details-tabs__top-item--active');
+        $(this).addClass('details-tabs__top-item--active');
+
+        $('.details-tabs__content-item').removeClass('details-tabs__content-item--active');
+        $($(this).attr('href')).addClass('details-tabs__content-item--active');
+    });
+
+    $('.details-item__num').styler();
+
+    $('.details-slide__thumb').slick({
+        asNavFor: '.details-slide__big',
+        focusOnSelect: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        vertical: true,
+        draggable: false,
+    });
+    $('.details-slide__big').slick({
+        asNavFor: '.details-slide__thumb',
+        draggable: false,
+        arrows: false,
+        fade: true
+    });
+
+
     $('.star').rateYo({
         starWidth: "11px",
         normalFill: "#d6d6d6",
